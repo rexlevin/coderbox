81aa21bdb18bd10b2fe989f8bb68482bfc5620f9
lizl6 <broodmantis@163.com>
2022-10-12 11:29:21 +0800

    hex编码/解码

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ const routes = [
         { type: 'node', name: 'ascii', title: '中文/ASCii互转', path: './component/ascii.html', state: 0 },
         { type: 'node', name: 'utf8', title: '中文/UTF-8编码互转', path: './component/utf8.html', state: 0 },
         { type: 'node', name: 'base64', title: 'Base64编码/解码', path: './component/base64.html', state: 0 },
+        { type: 'node', name: 'hex', title: 'Hex编码/解码', path: './component/hex.html', state: 0 },
         { type: 'node', name: 'aes', title: 'AES/DES/3DES', path: './component/aes.html', state: 0},
         { type: 'node', name: 'rsa', title: 'RSA非对称加解密/签名/验签', path: './component/rsa.html', state: 0}
     ]},
